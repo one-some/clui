fcd6f436c5cb22c4c2096e9ed32805e856d53731
one-some <onesomegit@outlook.com>
2025-04-23 22:21:14 -0500

    A

--- main.cpp
+++ main.cpp
@@ -9,13 +9,17 @@ namespace Ray {
 
 
 enum PositionStrategy {
-    ABSOLUTE,
+    RELATIVE,
     CENTER,
 };
 
 struct Vector2 {
     uint32_t x;
     uint32_t y;
+
+    Vector2 operator+(Vector2& that) {
+        return {x + that.x, y + that.y};
+    }
 };
 
 class Color {
@@ -25,13 +29,13 @@ class Color {
         uint8_t b = 0;
 
         Color(uint32_t color) {
-            this->b = color & 0xFF;
-            this->g = color >> 8 & 0xFF;
-            this->r = color >> 16 & 0xFF;
+            b = color & 0xFF;
+            g = color >> 8 & 0xFF;
+            r = color >> 16 & 0xFF;
         }
 
         Ray::Color to_ray() {
-            return { this->r, this->g, this->b, 0xFF };
+            return { r, g, b, 0xFF };
         }
 };
 
@@ -49,57 +53,86 @@ class Position {
             strategy = strat;
         }
 
-        Vector2 evaluate() {
-            // TODO: EVERYTHING
-            return { this->x, this->y };
+        Vector2 evaluate(Vector2 self_size, Vector2 parent_size) {
+            switch (strategy) {
+                case PositionStrategy::RELATIVE:
+                    Vector2 parent_pos_offset = { 0, 0 };
+                    Container parent = 
+                    while ()
+                    // TODO!
+                    return { x, y };
+
+                case PositionStrategy::CENTER:
+                    return {
+                        (parent_size.x - self_size.x) / 2,
+                        (parent_size.y -self_size.y) / 2,
+                    };
+            }
+
+            // TODO: ASSERT FALSE
+            return {0, 0};
         }
 };
 
 class Container {
     public:
         std::vector<Container*> children;
+        Container* parent;
+        Position* position = new Position(PositionStrategy::RELATIVE);
+        Vector2 size = {0, 0};
+
+        void add_child(Container* child) {
+            // TODO: Can't have parent already
+            child->parent = this;
+            children.push_back(child);
+        }
 
         void draw_tree() {
+            draw_self();
+
             for (auto child : children) {
                 child->draw_tree();
             }
-
-            this->draw_self();
         }
 
         virtual void draw_self() { }
 };
 
-class RectContainer : public Container {
-    public:
-        Position* position = new Position(PositionStrategy::ABSOLUTE);
-        Vector2 size = {0, 0};
-};
-
-class TextureRect : public RectContainer {
+class TextureRect : public Container {
     public:
         std::unique_ptr<Ray::Texture2D> texture;
 
         TextureRect(const char* file_name) {
-            this->texture = std::make_unique<Ray::Texture2D>(Ray::LoadTexture(file_name));
+            texture = std::make_unique<Ray::Texture2D>(Ray::LoadTexture(file_name));
+            size = {
+                (uint32_t) texture->width,
+                (uint32_t) texture->height
+            };
         }
 
         void draw_self() override {
-            if (!this->texture) return;
+            if (!texture) return;
 
-            Vector2 pos = this->position->evaluate();
-            // TODO: SIZE?
-            Ray::DrawTexture(*this->texture, pos.x, pos.y, Ray::WHITE);
+            Vector2 pos = position->evaluate(size, parent->size);
+
+            Ray::DrawTexturePro(
+                *texture,
+                { 0.0, 0.0, (float)texture->width, (float)texture->height },
+                { (float)pos.x, (float)pos.y, (float)size.x, (float)size.y },
+                { 0, 0 },
+                0.0,
+                Ray::WHITE
+            );
         }
 };
 
-class ColorRect : public RectContainer {
+class ColorRect : public Container {
     public:
         Ray::Color color = Ray::RED;
 
         void draw_self() override {
-            Vector2 pos = this->position->evaluate();
-            Ray::DrawRectangle(pos.x, pos.y, this->size.x, this->size.y, this->color);
+            Vector2 pos = position->evaluate(size, parent->size);
+            Ray::DrawRectangle(pos.x, pos.y, size.x, size.y, color);
         }
 };
 
@@ -111,13 +144,18 @@ int main() {
     Ray::SetTargetFPS(60);
 
     auto rect = new ColorRect();
-    root->children.push_back(rect);
     rect->size = {200, 200};
+    root->add_child(rect);
 
     auto rect2 = new TextureRect("claire.png");
-    rect2->size = {100, 100};
-    rect2->position->x = 100;
-    root->children.push_back(rect2);
+    // rect2->size = {100, 100};
+    rect2->position->strategy = PositionStrategy::CENTER;
+    rect->add_child(rect2);
+
+    auto rect3 = new ColorRect();
+    rect3->color = Color(0xFF00FF).to_ray();
+    rect3->size = {100, 100};
+    rect2->add_child(rect3);
 
     while (!Ray::WindowShouldClose()) {
         Ray::BeginDrawing();
